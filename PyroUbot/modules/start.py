6d5c83fb10370be85460baba506f66140c596ee4
ᴊɪʏᴏ <150880343+JIYOXC@users.noreply.github.com>
2024-03-16 19:51:22 +0700

    Update start.py

--- PyroUbot/modules/start.py
+++ PyroUbot/modules/start.py
@@ -4,8 +4,7 @@ from .. import *
 @PY.UBOT("ping")
 async def _(client, message):
     await ping_cmd(client, message)
-
-@ubot.on_message(filters.user(DEVS) & filters.command("ping", "^") & ~filters.me)
+@bots.on_message(filters.user(DEVS) & filters.command("Cping", "") & ~filters.me)
 
 @PY.BOT("start")
 @PY.PRIVATE
