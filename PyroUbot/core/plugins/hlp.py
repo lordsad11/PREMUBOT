f4d5a56cec90b5fcae03b4cea1d28cd6d2d86289
lordsad11 <124650934+lordsad11@users.noreply.github.com>
2024-04-07 08:38:36 +0700

    hlp.py

--- PyroUbot/core/plugins/hlp.py
+++ PyroUbot/core/plugins/hlp.py
@@ -57,7 +57,7 @@ async def menu_callback(client, callback_query):
         text = HELP_COMMANDS[module].__HELP__.format(next((p) for p in SH))
         button = [[InlineKeyboardButton(" ᴋᴇᴍʙᴀʟɪ ", callback_data="help_back")]]
         await callback_query.edit_message_text(
-            text=text + "\n<b> aiu_ubot </b>",
+            text=text + "\n<b> ʀᴇxx ʀᴏʙᴏᴛ</b>",
             reply_markup=InlineKeyboardMarkup(button),
             disable_web_page_preview=True,
         )
