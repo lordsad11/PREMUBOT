f51586e82eecbc30d5c2a47dd99f87f634df570f
lordsad11 <124650934+lordsad11@users.noreply.github.com>
2024-04-11 20:29:15 +0700

    ,

--- PyroUbot/modules/vctools.py
+++ PyroUbot/modules/vctools.py
@@ -1,95 +1,160 @@
-from asyncio import sleep
+import asyncio
 from contextlib import suppress
 from random import randint
 from typing import Optional
 
-from pyrogram import Client, enums, filters
+from pyrogram import enums
+from pyrogram.errors import *
 from pyrogram.raw.functions.channels import GetFullChannel
 from pyrogram.raw.functions.messages import GetFullChat
-from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
+from pyrogram.raw.functions.phone import (CreateGroupCall, DiscardGroupCall,
+                                          EditGroupCallTitle)
 from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
-from pyrogram.types import Message
 
 from PyroUbot import *
 
-MODULE = "vctools"
-HELP = f"""
-✘ Bantuan Untuk Voice Chat
+__modles__ = "Voicechat"
 
-๏ Perintah: <code>startvc</code>
-◉ Penjelasan: Untuk memulai voice chat grup.
+__help__ = get_cgr("help_vcs")
 
-๏ Perintah: <code>stopvc</code>
-◉ Penjelasan: Untuk mengakhiri voice chat grup.
-           
-๏ Perintah: <code>joinvc</code>
-◉ Penjelasan: Untuk bergabung voice chat grup.
+from pytgcalls import GroupCallFactory
+from pytgcalls.exceptions import GroupCallNotFoundError
 
-๏ Perintah: <code>leavevc</code>
-◉ Penjelasan: Untuk meninggalkan voice chat grup.
-"""
+klen_ = {}
 
 
-async def Ubot(
-    client: Client, message: Message, err_msg: str = ""
-) -> Optional[InputGroupCall]:
-    chat_peer = await client.resolve_peer(message.chat.id)
+class JoinVC:
+    def __init__(self, chat):
+        self._chat = chat
+        if klen_.get(chat):
+            self.group_call = klen_[chat]
+        else:
+            _client = GroupCallFactory(
+                nlx,
+                GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM,
+            )
+            self.group_call = _client.get_group_call()
+            klen_.update({chat: self.group_call})
+
+
+async def get_group_call(c: nlx, m, err_msg: str = "") -> Optional[InputGroupCall]:
+    em = Emojik()
+    em.initialize()
+    chat_peer = await c.resolve_peer(m.chat.id)
     if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
         if isinstance(chat_peer, InputPeerChannel):
-            full_chat = (await client.send(GetFullChannel(channel=chat_peer))).full_chat
+            full_chat = (await c.invoke(GetFullChannel(channel=chat_peer))).full_chat
         elif isinstance(chat_peer, InputPeerChat):
             full_chat = (
-                await client.send(GetFullChat(chat_id=chat_peer.chat_id))
+                await c.invoke(GetFullChat(chat_id=chat_peer.chat_id))
             ).full_chat
         if full_chat is not None:
             return full_chat.call
-    await eor(message, f"No group call Found {err_msg}")
+    await m.reply_text(cgr("vc_1").format(em.gagal, err_msg))
     return False
 
-list_data = []
 
-
-def remove_list(user_id):
-    list_data[:] = [item for item in list_data if item.get("id") != user_id]
-
-
-def add_list(user_id, text):
-    data = {"id": user_id, "nama": text}
-    list_data.append(data)
-
-
-def get_list():
-    if not list_data:
-        return "<b>ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴜsᴇʀ ᴅɪ ᴅᴀʟᴀᴍ ᴏʙʀᴏʟᴀɴ sᴜᴀʀᴀ ᴍᴀɴᴀᴘᴜɴ</b>"
-
-    msg = "\n".join(item["nama"] for item in list_data)
-    return msg
-
-
-@PY.UBOT("joinvc")
-async def _(client, message):
-    chat_id = message.command[1] if len(message.command) > 1 else message.chat.id
-    text = f"• <b>[{client.me.first_name} {client.me.last_name or ''}](tg://user?id={client.me.id})</b> | <code>{chat_id}</code>"
+@ky.ubot("startvc", sudo=True)
+async def _(c: nlx, m):
+    em = Emojik()
+    em.initialize()
+    flags = " ".join(m.command[1:])
+    ky = await m.reply(cgr("proses").format(em.proses))
+    if flags == enums.ChatType.CHANNEL:
+        chat_id = m.chat.title
+    else:
+        chat_id = m.chat.id
+    args = cgr("vc_2").format(em.sukses)
     try:
-        await client.group_call.start(chat_id, join_as=client.me.id)
+        await c.invoke(
+            CreateGroupCall(
+                peer=(await c.resolve_peer(chat_id)),
+                random_id=randint(10000, 999999999),
+            )
+        )
+        await ky.edit(args)
+        return
     except Exception as e:
-        return await message.reply(f"ERROR: {e}")
-    await message.reply("<b>izin parkir puh</b>")
-    await asyncio.sleep(5)
-    await client.group_call.set_is_mute(True)
-    add_list(client.me.id, text)
+        await ky.edit(cgr("err").format(em.gagal, e))
+        return
 
 
-@PY.UBOT("leavevc")
-async def _(client, message):
+@ky.ubot("stopvc", sudo=True)
+async def _(c: nlx, m):
+    em = Emojik()
+    em.initialize()
+    ky = await m.reply(cgr("proses").format(em.proses))
+    if not (group_call := (await get_group_call(c, m, err_msg=", Kesalahan..."))):
+        return
+    await c.invoke(DiscardGroupCall(call=group_call))
+    await ky.edit(cgr("vc_3").format(em.sukses))
+    return
+
+
+"""
+Ini Gw Bikin Dewek Ya Anj, Kalo Masih Dikata Copas Coba Cari Jing. ANAK KONTOL EMANG LOE PADA !!
+"""
+
+
+@ky.ubot("vctitle", sudo=True)
+async def _(c: nlx, m):
+    em = Emojik()
+    em.initialize()
+    txt = c.get_arg(m)
+    ky = await m.reply(cgr("proses").format(em.proses))
+    if len(m.command) < 2:
+        await ky.edit(cgr("vc_4").format(em.gagal, m.command))
+        return
+    if not (group_call := (await get_group_call(c, m, err_msg=", Kesalahan..."))):
+        return
     try:
-        await client.group_call.stop()
-    except Exception as e:
-        return await message.reply(f"ERROR: {e}")
-    remove_list(client.me.id)
-    return await message.reply("<b>izin turun puh</b>")
+        await c.invoke(EditGroupCallTitle(call=group_call, title=f"{txt}"))
+    except Forbidden:
+        await ky.edit(cgr("vc_5").format(em.gagal))
+        return
+    await ky.edit(cgr("vc_6").format(em.sukses, txt))
+    return
 
 
-@PY.UBOT("listos")
-async def _(client, message):
-    await message.reply(get_list())
+@ky.ubot("joinvc", sudo=True)
+@ky.devs("Jvcs")
+async def _(c: nlx, m):
+    em = Emojik()
+    em.initialize()
+
+    ky = await m.reply(cgr("proses").format(em.proses))
+    chat_id = m.command[1] if len(m.command) > 1 else m.chat.id
+    with suppress(ValueError):
+        chat_id = int(chat_id)
+    if chat_id:
+        Nan = JoinVC(chat_id)
+        try:
+            await Nan.group_call.join(chat_id)
+            await ky.edit(cgr("vc_7").format(em.sukses, chat_id))
+            await asyncio.sleep(2)
+            await Nan.group_call.set_is_mute(True)
+            return
+        except GroupCallNotFoundError as e:
+            return await ky.edit(cgr("err").format(em.gagal, e))
+
+
+@ky.ubot("leavevc", sudo=True)
+@ky.devs("Lvcs")
+async def _(c: nlx, m):
+    em = Emojik()
+    em.initialize()
+    ky = await m.reply(cgr("proses").format(em.proses))
+    chat_id = m.command[1] if len(m.command) > 1 else m.chat.id
+    with suppress(ValueError):
+        chat_id = int(chat_id)
+    if chat_id:
+        Nan = JoinVC(chat_id)
+        try:
+            await Nan.group_call.leave()
+            await ky.edit(cgr("vc_9").format(em.sukses, chat_id))
+            return
+        except Exception as e:
+            await ky.edit(cgr("err").format(em.gagal, e))
+            return
+    else:
+        return ky.edit(cgr("vc_10").format(em.gagal))
