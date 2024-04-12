a0cf3ba510a1b0f69986e6fd42d64f7d8c566c86
lordsad11 <124650934+lordsad11@users.noreply.github.com>
2024-04-12 16:00:55 +0700

    Add files via upload

---
+++ PyroUbot/modules/afk.py
@@ -0,0 +1,17 @@
+
+
+from ubot import *
+
+__MODULE__ = "Afk"
+__HELP__ = """
+Bantuan Untuk Afk
+
+• Perintah: <code>{0}afk</code> [alasan]
+• Penjelasan: Untuk mengaktifkan mode afk.
+"""
+
+
+@PY.UBOT("afk")
+async def _(client, message):
+    await set_afk(client, message)
+    
