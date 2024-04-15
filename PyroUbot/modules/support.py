ec966933a9e4ac26b21fc28baa6367bfd295bf02
lordsad11 <124650934+lordsad11@users.noreply.github.com>
2024-04-15 07:45:55 +0700

    support.py

--- PyroUbot/modules/support.py
+++ PyroUbot/modules/support.py
@@ -4,4 +4,4 @@ from PyroUbot import *
 
 @PY.CALLBACK("profil")
 async def _(client, callback_query):
-    await profilcallback(client, callback_query)
+    await profil_callback(client, callback_query)
